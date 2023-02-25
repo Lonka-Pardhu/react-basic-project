06ee747dd6f3e31dd232a1e3b2a6808a026780d8
Lonkapardhu <pardhugadichannel99@gmail.com>
2023-02-25 14:20:25 +0530

    added project files

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+
+        <nav className="nav">
+            <img src="/src/react-logo.png" alt="reactlogo" className="nav--icon" />
+            <h1 className="nav--logo-text">ReactFacts</h1>
+            <h3 className="nav--title">React Course - Project 1</h3>
+        </nav>
+
+    )
+}
